df348f359c981b1b50c02cbcb469e59ccb381282
Farkhod Tursunbaev <foha8807@hotmail.com>
2020-08-07 18:39:48 -0400

    lesson02 commits easy homework on lesson02.js file

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,23 @@
-//this is single line comment
-/*
-    this is homework 01Lesson
-    below pop up alert message
-    this is multiple line comment
-*/
+// //this is single line comment
+// /*
+//     this is homework 01Lesson
+//     below pop up alert message
+//     this is multiple line comment
+// */
 
-alert("Welcome to Homework 01");
-// we can use var, let const to create variables.
-// creating variables
-const money = 5;
-const income = 10;
-var addExpenses = 9;
-const deposit = 20;
-var mission = 7;
-let period = 3;
+// alert("Welcome to Homework 01");
+// // we can use var, let const to create variables.
+// // creating variables
+// // const money = 5;
+// // const income = 10;
+// // var addExpenses = 9;
+// // const deposit = 20;
+// // var mission = 7;
+// // let period = 3;
 
-alert("I have "+money+" $. My todays income is"+income+" $.");
+// // alert("I have "+money+" $. My todays income is"+income+" $.");
 
-//console.log message
+// // //console.log message
 
-console.log((money + income) - addExpenses);
-console.log(period);
+// // console.log((money + income) - addExpenses);
+// // console.log(period);
---
+++ JS/lesson02.js
@@ -0,0 +1,18 @@
+let money = 8800;
+let income = 'Web-Developer';
+let addExpenses = "internEt, trANsportation, FOod, grocerY";
+let deposit = true;
+let mission = 100000;
+let period = 5;
+let budgetDay; 
+budgetDay = (money / 30);
+//type of variables
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses[3]);
+console.log(budgetDay);
