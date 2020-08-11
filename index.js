4a336110652d0b9b987c8436b1fd82a0f6cff53e
Farkhod Tursunbaev <foha8807@hotmail.com>
2020-08-11 03:54:39 -0400

    lesson03 second commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-//this is single line comment
-/*
-    this is homework 01Lesson
-    below pop up alert message
-    this is multiple line comment
-*/
+'use strict';
 
-// alert("Welcome to Homework 01");
-// we can use var, let const to create variables.
-// creating variables
-let money;
+let money1 = +prompt("Ваш месячный доход?");
+let money2 = +prompt("Ваш месячный доход?");
 let income;
-var addExpenses;
+var addExpenses1 = +prompt("Перечислите возможные расходы");
+var addExpenses2 = +prompt("Перечислите возможные расходы");
 let deposit;
-var mission;
 let period;
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(addExpenses));
-console.log(typeof(deposit));
-console.log(typeof(mission));
-console.log(typeof(period));
+// console.log(typeof(money));
+// console.log(typeof(income));
+// console.log(typeof(addExpenses));
+// console.log(typeof(deposit));
+// console.log(typeof(mission));
+// console.log(typeof(period));
+
+let mon1 = console.log('expenses1: ', money1);
+let mon2 = console.log('expenses2: ', money2);
+let exp1 = console.log('amount1: ', addExpenses1);
+let exp2 = console.log('amount2: ', addExpenses2);
+console.log(confirm("Есть ли у вас депозит в банке?"));
+const budgetMonth = console.log('бюджет на месяц: ', ((money1+money2)-(addExpenses1+addExpenses2)));
+const mission = Math.max(budgetMonth * 12);
+console.log('за сколько месяцев', mission);
