53aa755408693dec840f30ae35102507f97efb41
Farkhod Tursunbaev <foha8807@hotmail.com>
2020-08-12 07:53:02 -0400

    lesson03 final commit

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 'use strict';
 
-let money1 = +prompt("Ваш месячный доход?");
-let money2 = +prompt("Ваш месячный доход?");
+let money = +prompt("Ваш месячный доход?", 9900);
 let income;
-var addExpenses1 = +prompt("Перечислите возможные расходы");
-var addExpenses2 = +prompt("Перечислите возможные расходы");
-let deposit;
-let period;
+let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+let deposit = confirm("Есть ли у вас депозит в банке?");
+let mission = 40000;
+let period = 4;
 
-// console.log(typeof(money));
-// console.log(typeof(income));
-// console.log(typeof(addExpenses));
-// console.log(typeof(deposit));
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+// console.log(income.length);
 // console.log(typeof(mission));
-// console.log(typeof(period));
+// console.log(typeof addExpenses);
 
-let mon1 = console.log('expenses1: ', money1);
-let mon2 = console.log('expenses2: ', money2);
-let exp1 = console.log('amount1: ', addExpenses1);
-let exp2 = console.log('amount2: ', addExpenses2);
-console.log(confirm("Есть ли у вас депозит в банке?"));
-const budgetMonth = console.log('бюджет на месяц: ', ((money1+money2)-(addExpenses1+addExpenses2)));
-const mission = Math.max(budgetMonth * 12);
-console.log('за сколько месяцев', mission);
+let exp1 = prompt("Введите обязательную статью расходов?", "School expenses");
+let amnt1 = +prompt("Во сколько это обойдется?", 3200);
+let exp2 = prompt("Введите обязательную статью расходов?", "College Expenses");
+let amnt2 = +prompt("Во сколько это обойдется?", 4700);
+
+console.log("Period " + + " of month");
+console.log(`Goal to earn ${mission} USDOllars`);
+console.log(addExpenses.toLowerCase().split(","));
+
+let expAmnt = amnt1 + amnt2;
+console.log(expAmnt);
+let budgetMonth = (money - (expAmnt));
+console.log("Monthly Income: " + budgetMonth);
+let periodMission = Math.ceil(mission / budgetMonth);
+let budgetDay = budgetMonth / 30;
+
+console.log("Daily Budget: " + Math.floor(budgetDay));
+console.log("Goal will be achieved in: " + periodMission);
+
+if (budgetDay < 300) {
+    console.log("К сожалению у вас уровень дохода ниже среднего");
+} else if(budgetDay <=800) {
+    console.log("У вас средний уровень дохода");
+}else{
+    console.log("У вас высокий уровень дохода");
+}
