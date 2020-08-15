d6da5a2155c723b58d5aaf04025b19557858069c
Farkhod Tursunbaev <foha8807@hotmail.com>
2020-08-14 20:56:19 -0400

    first commit of lesson05

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
 'use strict';
 
-let money = +prompt("Ваш месячный доход?", 9900);
-let income;
-let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-let deposit = confirm("Есть ли у вас депозит в банке?");
-let mission = 40000;
-let period = 4;
+let isNum = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+};
 
-// console.log(income.length);
-// console.log(typeof(mission));
-// console.log(typeof addExpenses);
+let money,
+    income = "Фриланс",
+    addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
+    deposit = confirm("Есть ли у вас депозит в банке?"),
+    mission = 40000,
+    period = 4;
 
-let exp1 = prompt("Введите обязательную статью расходов?", "School expenses");
-let amnt1 = +prompt("Во сколько это обойдется?", 3200);
-let exp2 = prompt("Введите обязательную статью расходов?", "College Expenses");
-let amnt2 = +prompt("Во сколько это обойдется?", 4700);
+let start = function(){
+    money = prompt("Ваш месячный доход?");
+    while (!isNum(money)){
+        money = prompt("Ваш месячный доход?");
+    }
+};
+start();
 
+
+const showTypeof = function(item){
+    console.log(typeof item);
+};
+showTypeof(money);
+showTypeof(income);
+showTypeof(deposit);
+
+let expenses =[];
 console.log(addExpenses.toLowerCase().split(","));
 
-// 5) Удалить из кода переменную budgetMonth
+let getExpensesMonth = function() {
+    let sum = 0;
 
-/* let expAmnt = amnt1 + amnt2;
-console.log(expAmnt);
-let budgetMonth = (money - (expAmnt));
-console.log("Monthly Income: " + budgetMonth);
-let periodMission = Math.ceil(mission / budgetMonth);
-let getStatusIncome = budgetMonth / 30;
-
-console.log("Daily Budget: " + Math.floor(getStatusIncome));
- console.log("Goal will be achieved in: " + periodMission);
-*/
-
-// 1) Объявить функцию getExpensesMonth.
-const getExpensesMonth = function() {
-    return amnt1 + amnt2;
+    for (let i=0; i<4; i++){
+        expenses[i] = prompt("Введите обязательную статью расходов?");
+        sum += +prompt("Во сколько это обойдется?");
+    }
+    console.log(expenses);
+    return sum;
 };
-console.log("Функция возвращает сумму всех обязательных расходов за месяц: " + getExpensesMonth());
+let expensesAmount = getExpensesMonth();
+console.log("Расходы за месяц: " + expensesAmount);
 
-// 2) Объявить функцию getAccumulatedMonth.
 const getAccumulatedMonth = function() {
-  return money - getExpensesMonth();
+  return money - expensesAmount;
 };
 console.log("Накопления за месяц: " + getAccumulatedMonth());
 
-// 3) Объявить переменную accumulatedMonth
 let accumulatedMonth = getAccumulatedMonth();
 
-// 4) Объявить функцию getTargetMonth.
 const getTargetMonth = function() {
     return mission / accumulatedMonth
 };
-console.log("за какой период будет достигнута цель: " + getTargetMonth());
+console.log("за какой период будет достигнута цель: " + Math.floor(getTargetMonth()));
 
-// 6) budgetDay высчитываем исходя из значения месячного накопления (accumulatedMonth)
 let getStatusIncome = accumulatedMonth / 30;
 console.log("высчитываем исходя из значения месячного накопления: " + Math.ceil(getStatusIncome));
 
@@ -62,12 +64,4 @@ if (getStatusIncome < 300) {
     console.log("У вас средний уровень дохода");
 }else{
     console.log("У вас высокий уровень дохода");
-}
-
-// 7)- вызовы функции showTypeOf
-const showTypeof = function(item){
-    console.log(typeof item);
 };
-showTypeof(money);
-showTypeof(income);
-showTypeof(deposit);
